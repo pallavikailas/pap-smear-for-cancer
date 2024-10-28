9b70d93cb3c1187f41b25153f793772f56cf0153
Pallavi Kailas <119298061+pallavikailas@users.noreply.github.com>
2024-10-29 01:06:56 +0530

    Create train.py

---
+++ src/train.py
@@ -0,0 +1,34 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from data_loader import get_data
+
+def train_random_forest(X_train, y_train):
+    rf = RandomForestClassifier(n_estimators=100, random_state=42)
+    rf.fit(X_train, y_train)
+    return rf
+
+def train_knn(X_train, y_train):
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(X_train, y_train)
+    return knn
+
+def train_svm(X_train, y_train):
+    svm = SVC(kernel="linear", probability=True)
+    svm.fit(X_train, y_train)
+    return svm
+
+if __name__ == "__main__":
+    X, y = get_data("data/train")
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    rf_model = train_random_forest(X_train, y_train)
+    knn_model = train_knn(X_train, y_train)
+    svm_model = train_svm(X_train, y_train)
+
+    # Save models if needed
+    # import joblib
+    # joblib.dump(rf_model, '../models/random_forest.pkl')
+    # joblib.dump(knn_model, '../models/knn.pkl')
+    # joblib.dump(svm_model, '../models/svm.pkl')
