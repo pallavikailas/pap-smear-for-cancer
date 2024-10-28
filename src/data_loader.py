48aabc1172413f09cd6aa9221fe95d794de7b348
Pallavi Kailas <119298061+pallavikailas@users.noreply.github.com>
2024-10-29 01:02:03 +0530

    Create data_loader.py

---
+++ src/data_loader.py
@@ -0,0 +1,19 @@
+import os
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def get_dataloaders(data_dir, batch_size=32):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    train_dataset = datasets.ImageFolder(os.path.join(data_dir, 'train'), transform=transform)
+    val_dataset = datasets.ImageFolder(os.path.join(data_dir, 'validation'), transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, val_loader
