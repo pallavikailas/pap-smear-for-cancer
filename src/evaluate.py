29610be1a48bc1983a7aa7d6d8e650d6053390ad
Pallavi Kailas <119298061+pallavikailas@users.noreply.github.com>
2024-11-06 13:58:47 +0530

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,28 +1,3 @@
-# from sklearn.metrics import accuracy_score, classification_report
-# from train import train_random_forest, train_knn, train_svm
-# from data_loader import get_data
-
-# def evaluate_model(model, X_val, y_val):
-#     preds = model.predict(X_val)
-#     accuracy = accuracy_score(y_val, preds)
-#     report = classification_report(y_val, preds)
-#     print(f"Accuracy: {accuracy * 100:.2f}%")
-#     print("Classification Report:\n", report)
-
-# if __name__ == "__main__":
-#     X, y = get_data("data/validation")
-
-#     rf_model = train_random_forest(X, y)
-#     knn_model = train_knn(X, y)
-#     svm_model = train_svm(X, y)
-
-#     print("Random Forest:")
-#     evaluate_model(rf_model, X, y)
-#     print("KNN:")
-#     evaluate_model(knn_model, X, y)
-#     print("SVM:")
-#     evaluate_model(svm_model, X, y)
-
 from sklearn.metrics import classification_report, f1_score
 from train import train_random_forest, train_knn, train_svm
 from data_loader import get_data
@@ -47,4 +22,4 @@ if __name__ == "__main__":
     evaluate_model(knn_model, X, y)
     print("SVM:")
     evaluate_model(svm_model, X, y)
-
+    
